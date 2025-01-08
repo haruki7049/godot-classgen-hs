e5f0ef1bc2b606d3603191ace1def6c0055ebe2a
haruki7049 <tontonkirikiri@gmail.com>
2025-01-08 16:43:34 +0900

    build(flakes): Init

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    stacklock2nix.url = "github:cdepillabout/stacklock2nix";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          overlays = [
+            inputs.stacklock2nix.overlay
+          ];
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system overlays;
+          };
+        };
+    };
+}
