b10d68e7ff4210a1176dbe6bbd38314b8a83d11b
haruki7049 <tontonkirikiri@gmail.com>
2025-01-09 20:07:13 +0900

    Init stacklock2nix (#1)
    * chore(flakes): Add stacklock2nix to inputs
    
    * feat!(stacklock2nix): Init
    
    * chore(stacklock2nix): Delete unneeded dontCheck
    
    * refactor(flakes): Change classgen to classgen-stacklock

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
+    stacklock2nix.url = "github:cdepillabout/stacklock2nix";
     flake-compat.url = "github:edolstra/flake-compat";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
@@ -16,13 +17,52 @@
 
       perSystem =
         { pkgs, system, ... }:
+        let
+          overlays = [
+            inputs.stacklock2nix.overlay
+            (final: prev: {
+              classgen-stacklock = final.stacklock2nix {
+                stackYaml = ./stack.yaml;
+                baseHaskellPkgSet = final.haskell.packages.ghc984;
+
+                additionalDevShellNativeBuildInputs = stacklockHaskellPkgSet: [
+                  final.stack
+                  final.nil
+                ];
+
+                cabal2nixArgsOverrides = args: args // {
+                  prettyprinter = version: { pgp-wordlist = null; };
+                };
+
+                additionalHaskellPkgSetOverrides = hfinal: hprev: {
+                  integer-logarithms = final.haskell.lib.compose.dontCheck hprev.integer-logarithms;
+                  prettyprinter = final.haskell.lib.compose.dontCheck hprev.prettyprinter;
+                  indexed-traversable-instances = final.haskell.lib.compose.dontCheck hprev.indexed-traversable-instances;
+                  integer-conversion = final.haskell.lib.compose.dontCheck hprev.integer-conversion;
+                  text-iso8601 = final.haskell.lib.compose.dontCheck hprev.text-iso8601;
+                };
+
+                all-cabal-hashes = final.fetchFromGitHub {
+                  owner = "commercialhaskell";
+                  repo = "all-cabal-hashes";
+                  rev = "df4fd6587f7e97d8170250ba4419f2cb062736c4";
+                  hash = "sha256-n6VzAT87v2DrMNDsmJXYMTf1a2hhtyx/V5CJkix0cYk=";
+                };
+              };
+            })
+          ];
+        in
         {
-          devShells.default = pkgs.mkShell {
-            packages = [
-              pkgs.nil
-              pkgs.stack
-            ];
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system overlays;
+            config.allowBroken = true;
           };
+
+          packages = {
+            default = pkgs.classgen-stacklock.pkgSet.godot-haskell-classgen;
+          };
+
+          devShells.default = pkgs.classgen-stacklock.devShell;
         };
     };
 }
